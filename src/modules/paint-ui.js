0cdff9e0fe4b79ec015fa5cc1d7023cd3818dbce
veronica365 <naruth10@gmail.com>
2023-02-22 10:17:39 +0300

    Add functionality to create a new todo to the list

--- src/modules/paint-ui.js
+++ src/modules/paint-ui.js
@@ -1,4 +1,10 @@
-import { returnHead, returnTodoForm, generateTodos } from "./components";
+import todos from "./todos";
+import {
+  returnHead,
+  returnTodoForm,
+  generateTodos,
+  returnTodo,
+} from "./components";
 
 export const appendHTML = () => {
   const element = document.createElement("div");
@@ -6,7 +12,48 @@ export const appendHTML = () => {
   return element;
 };
 
+const getTodoInput = () => {
+  return document.querySelector("form input");
+};
+
+export const createNewTodo = (description) => {
+  let html = document.querySelector(".todo-wrapper");
+  const newTodo = {
+    description,
+    selected: false,
+    completed: false,
+    index: todos.length + 1,
+  };
+  todos.push(newTodo);
+  html.innerHTML += returnTodo(newTodo);
+};
+
+export const addTodo = () => {
+  const input = getTodoInput();
+  const submit = document.querySelector("form button");
+  let hasText = "";
+  ["click", "focus", "change"].forEach((event) => {
+    input.addEventListener(event, () => {
+      hasText = input.value.trim();
+      submit.setAttribute("disabled", true);
+      if (!hasText) return;
+      submit.removeAttribute("disabled");
+    });
+  });
+};
+
+export const submitTodo = (event) => {
+  event.preventDefault();
+  const input = getTodoInput();
+  const description = input.value.trim();
+  if (!description) return;
+  createNewTodo(description);
+  getTodoInput().value = "";
+};
+
 export const initialize = () => {
   document.querySelector(".todos").prepend(appendHTML());
   document.querySelector(".todo-wrapper").innerHTML = generateTodos();
+  document.querySelector("form").addEventListener("submit", submitTodo);
+  addTodo();
 };
