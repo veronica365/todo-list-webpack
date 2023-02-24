8b25cb6d390dab1f48d792c0764c66b574b3aa9b
veronica365 <naruth10@gmail.com>
2023-02-24 13:07:31 +0300

    Add new file in the modules folder

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,5 +1,5 @@
 export default class Todos {
-  static todo = "todos-todo-list-webpack-veronica365-2023";
+  static todo = 'todos-todo-list-webpack-veronica365-2023';
 
   static setTodo(todos) {
     const newTodos = JSON.stringify(todos);
@@ -17,7 +17,7 @@ export default class Todos {
     return todos || [];
   }
 
-  static removeData(id = "", allCompleted = false) {
+  static removeData(id = '', allCompleted = false) {
     let todos = this.listData();
     if (allCompleted) {
       todos = todos.filter((todo) => !todo.completed);
